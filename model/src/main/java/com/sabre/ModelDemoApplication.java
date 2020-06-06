7cb68469ef5d8d7d1370db867d76cff94f56b63c
ebiz_ <ebiz_@192.168.0.21>
2020-06-06 12:59:00 -0500

    Refine POM and commit model

---
+++ model/src/main/java/com/sabre/ModelDemoApplication.java
@@ -0,0 +1,15 @@
+package com.sabre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages="com.sabre")
+public class ModelDemoApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelDemoApplication.class);
+
+	}
+
+}
