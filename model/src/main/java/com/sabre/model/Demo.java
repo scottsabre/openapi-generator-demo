ffe18e8a7db1881b72474ac99a8c26b63bd03fb6
Scott Sabre <d.scott.sabre@gmail.com>
2020-06-06 14:06:37 -0500

    Regenerate Model

--- model/src/main/java/com/sabre/model/Demo.java
+++ model/src/main/java/com/sabre/model/Demo.java
@@ -1,10 +1,11 @@
 /**
- * Authored 2020 by D. Scott Sabre (d.scott.sabre@gmail.com). All rights reserved. This software
- * is proprietary property owned by D. Scott Sabre. This software is available for use by all
- * (the user), whether for personal or commercial use. However if this software is reused
- * in a manner where the contents remain wholly or "substantially" intact, the user agrees to
- * keep this author's statement in that content. The user shall shall use this software only in
- * accordance with these terms.
+ * @author D. Scott Sabre
+ * @since 2020
+ * All rights reserved. This software is proprietary property owned by the author. This software is fully
+ * available for reuse, however if the contents remain wholly or substantially intact, the user agrees
+ * to keep this statement in that content. This software is offered â€œas-isâ€? and without warranty, and the
+ * author assumes no liability for damages resulting from use. The user shall shall use this software only
+ * in accordance with these terms.
  */
 /*
  * OpenApi Generator Demo
@@ -39,7 +40,7 @@ import java.util.List;
 /**
  * Demo
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-06-06T13:42:58.149-05:00[America/Chicago]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-06-06T14:05:09.393-05:00[America/Chicago]")
 public class Demo {
   public static final String SERIALIZED_NAME_ID = "id";
   @SerializedName(SERIALIZED_NAME_ID)
--- model/src/test/java/com/sabre/openapigeneratordemo/OpenapiGeneratorDemoApplicationTests.java
+++ model/src/test/java/com/sabre/openapigeneratordemo/OpenapiGeneratorDemoApplicationTests.java
@@ -1,4 +1,39 @@
 /**
+ * @author D. Scott Sabre
+ * @since 2020
+ * All rights reserved. This software is proprietary property owned by the author. This software is fully
+ * available for reuse, however if the contents remain wholly or substantially intact, the user agrees
+ * to keep this statement in that content. This software is offered â€œas-isâ€? and without warranty, and the
+ * author assumes no liability for damages resulting from use. The user shall shall use this software only
+ * in accordance with these terms.
+ */
+/**
+ * @author D. Scott Sabre
+ * @since 2020
+ * All rights reserved. This software is proprietary property owned by the author. This software is fully
+ * available for reuse, however if the contents remain wholly or substantially intact, the user agrees
+ * to keep this statement in that content. This software is offered â€œas-isâ€? and without warranty, and the
+ * author assumes no liability for damages resulting from use. The user shall shall use this software only
+ * in accordance with these terms.
+ */
+/**
+ * @author D. Scott Sabre
+ * @since 2020
+ * All rights reserved. This software is proprietary property owned by the author. This software fully
+ * available for reuse, however if the contents remain wholly or substantially intact, the user agrees
+ * to keep this author's statement in that content. This software is offered â€œas-isâ€? and without warranty,
+ * and the author assumes no liability for damages resulting from use. The user shall shall use this
+ * software only in accordance with these terms.
+ */
+/**
+ * @author D. Scott Sabre
+ * @since 2020
+ * All rights reserved. This software is proprietary property owned by the author. This software fully
+ * available for reuse, however if the contents remain wholly or substantially intact, the user agrees
+ * to keep this author's statement in that content. This sofware is offered â€œas-isâ€? and without warranty, and the author assumes no liability for damages resulting from use.
+ * The user shall shall use this software only in accordance with these terms.
+ */
+/**
  * Authored 2020 by D. Scott Sabre (d.scott.sabre@gmail.com). All rights reserved. This software
  * is proprietary property owned by D. Scott Sabre. This software is available for use by all
  * (the user), whether for personal or commercial use. However if this software is reused
