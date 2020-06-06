66fc2615a97bb1e3713fa25503b029f9e2c0aa16
scottsabre <66492922+scottsabre@users.noreply.github.com>
2020-06-06 11:05:35 -0500

    Initial Commit from Spring Initializr

---
+++ openapi-generator-demo/src/main/java/com/sabre/openapigeneratordemo/OpenapiGeneratorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sabre.openapigeneratordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiGeneratorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiGeneratorDemoApplication.class, args);
+	}
+
+}
---
+++ openapi-generator-demo/src/test/java/com/sabre/openapigeneratordemo/OpenapiGeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabre.openapigeneratordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGeneratorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
